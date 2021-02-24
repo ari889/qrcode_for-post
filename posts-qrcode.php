917176a8338cf44f43473ee4ba905d9a322eefe8
ari889 <arijitbanarjee889@gmail.com>
2021-02-24 13:47:28 +0600

    Make plugin developer friendly

--- posts-qrcode.php
+++ posts-qrcode.php
@@ -20,8 +20,29 @@ function pqrc_display_qr_code($content){
     $current_post_id = get_the_ID();
     $current_post_title = get_the_title($current_post_id);
     $current_post_url = urlencode(get_the_permalink($current_post_id));
-    $image_src = sprintf('https://api.qrserver.com/v1/create-qr-code/?data=%s&size=220x220&margin=0', $current_post_url);
-    $content .= sprintf("<div class='qrcode'><img src='%s' alt='%s' /></div>", $image_src, $current_post_title);
+    $current_post_type = get_post_type($current_post_id);
+
+    /**
+     * post type check
+     */
+    $excluded_post_type = apply_filters("pqrc_excluded_post_types", array());
+
+    if(in_array($current_post_type, $excluded_post_type)){
+        return $content;
+    }
+
+    /**
+     * size hook
+     */
+    $dimention = apply_filters('pqrc_qucode_dimention', '185x185');
+
+    /**
+     * image attributes
+     */
+    $image_attributes = apply_filters('pqrc_image_attributes', null);
+
+    $image_src = sprintf('https://api.qrserver.com/v1/create-qr-code/?data=%s&size=%s&margin=0', $current_post_url, $dimention);
+    $content .= sprintf("<div class='qrcode'><img %s src='%s' alt='%s' /></div>", $image_attributes, $image_src, $current_post_title);
     return $content;
 }
 
